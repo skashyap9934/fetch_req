48c539693cc3330c1048e5fc9553f9407251454c
skashyap9934 <skashyap9934@gmail.com>
2024-06-15 21:10:08 +0530

    fetch

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/products", async (req, res) => {
   res.status(200).json({ products: products });
 });
 
-app.get("/", () => {
+app.get("/", (req, res) => {
   res.json({ message: "Homepage" });
 });
 
