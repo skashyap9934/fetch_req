17b0e03f06ff55eb3c80ed344418b46ff7aa7da5
skashyap9934 <skashyap9934@gmail.com>
2024-06-16 13:53:44 +0530

    first commit

---
+++ fetch_two/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
