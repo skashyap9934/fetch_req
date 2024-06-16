831ba33eccbd8b363ecb33f14a7dd59f6ff3e295
skashyap9934 <skashyap9934@gmail.com>
2024-06-16 16:56:15 +0530

    walmart backend

--- walmart/backend/index.js
+++ walmart/backend/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 // Variables
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use("/products", productsRouter);
