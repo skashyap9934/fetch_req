fb424f61a4499e63a1009fd4c0c3855abfb1ded9
skashyap9934 <skashyap9934@gmail.com>
2024-06-16 16:19:47 +0530

    walmart backend

--- fetch_two/index.js
+++ fetch_two/index.js
@@ -7,6 +7,8 @@ const { wishlistsRouter } = require("./routers/wishlist.router");
 const bagsRouter = require("./routers/bags.router");
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -15,10 +17,20 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://walmart-clone-frontend.vercel.app/",
+    methods: ["GET", "POST", "DELETE"],
   })
 );
+
+const temp = express.Router();
+
+temp.get("/", async (req, res) => {
+  const collection = mongoose.connection.collection("products");
+  const products = await collection.find().limit(11).toArray();
+  res.json({ products: products });
+});
+
 app.use("/user", userRouter);
-app.use("/products", productsRouter);
+app.use("/products", temp);
 app.use("/wishlist", wishlistsRouter);
 app.use("/bag", bagsRouter);
 
