148cc506f08ec52023d6e5eddfc3bdf39bcd3df3
skashyap9934 <skashyap9934@gmail.com>
2024-06-16 14:03:07 +0530

    first commit

--- fetch_two/index.js
+++ fetch_two/index.js
@@ -1,12 +1,16 @@
 // index.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/products", (req, res) => {
+  res.send("Products Page");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
